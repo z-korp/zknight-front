4b1aba519ee906280444d9e6b905030a8a0bf861
Matthias Monnier <matthias.monnier@gmail.com>
2023-09-03 21:02:24 +0200

    fix mx

--- client/src/ui/ResetButton.tsx
+++ client/src/ui/ResetButton.tsx
@@ -10,7 +10,7 @@ const ResetButton: React.FC<NewGameButtonProps> = ({ onClick, disabled }) => {
     <button
       className={`bg-blue-500 ${
         disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-700'
-      } my-10 text-white font-bold py-2 px-4 rounded`}
+      } mx-10 my-10 text-white font-bold py-2 px-4 rounded`}
       style={{ width: '160px' }}
       onClick={onClick}
       disabled={disabled}
