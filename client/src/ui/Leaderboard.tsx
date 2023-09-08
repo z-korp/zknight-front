8bdce49c5774337d69bec3daba1722bc934e0cbf
Matthias Monnier <matthias.monnier@gmail.com>
2023-09-08 15:39:18 +0200

    Refactor leaderboard layout and logging statements

--- client/src/ui/Leaderboard.tsx
+++ client/src/ui/Leaderboard.tsx
@@ -15,16 +15,21 @@ const Leaderboard = () => {
     .map((player) => ({ player, score: playerScores[player] }))
     .sort((a, b) => b.score - a.score);
 
+  console.log('sortedPlayers', sortedPlayers);
+
   // Split the players into two columns
   const leftPlayers = sortedPlayers.slice(0, 10);
   const rightPlayers = sortedPlayers.slice(10);
+
+  console.log(leftPlayers);
+  console.log(rightPlayers);
 
   return (
     <>
       <h1 className="text-black mb-4 text-center">Leaderboard</h1>
       <hr className="my-4 border-2" />
       <div className="flex justify-center">
-        <div className="flex flex-col items-center justify-between w-1/2">
+        <div className="flex flex-col items-center justify-start w-1/2">
           {leftPlayers.map((entry, index) => (
             <div key={index} className="flex justify-around w-full py-2 text-black">
               <span className="w-10 text-right">{index + 1}.</span>
@@ -34,7 +39,7 @@ const Leaderboard = () => {
           ))}
         </div>
         <div className="my-4 border-l-2 mx-2 h-full"></div> {/* This is the vertical divider */}
-        <div className="flex flex-col items-center justify-between w-1/2">
+        <div className="flex flex-col items-center justify-start w-1/2">
           {rightPlayers.map((entry, index) => (
             <div key={index + 10} className="flex justify-around w-full py-2 text-black">
               <span className="w-10 text-right">{index + 11}.</span>
