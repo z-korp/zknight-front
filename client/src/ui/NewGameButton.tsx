e26c01432ab9dd5a9ec2df5584dd3a4f5592e03a
Cheelax <thomas.belloc@gmail.com>
2023-09-22 09:46:50 +0200

    ✨ feat: Add credits button and modal functionality in App.tsx
    - Added `CreditsButton` component in `client/src/ui/CreditsButton.tsx`.
    - Imported `CreditsButton` component in `client/src/App.tsx`.
    - Updated JSX in `App` component to include the `CreditsButton` component.
    - Created a `modal-medium` CSS class in `client/src/index.css` for the styling of the modal.
    - Added `Modal` component from 'react-modal' library in `App` component to show a modal when the credits button is clicked.
    - Added a close button in the modal header to close the modal.
    
    Co-authored-by: [Your Name] <[Your Email]>

--- client/src/ui/NewGameButton.tsx
+++ client/src/ui/NewGameButton.tsx
@@ -10,8 +10,7 @@ const NewGameButton: React.FC<NewGameButtonProps> = ({ onClick, disabled }) => {
     <button
       className={`bg-blue-500 ${
         disabled ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-700'
-      } my-10 text-white font-bold py-2 px-4 rounded`}
-      style={{ width: '100%' }}
+      } my-10 text-white font-bold py-2 px-4 rounded w-full`}
       onClick={onClick}
       disabled={disabled}
     >
